074504c98effb7ae9c1ed585e4d912b1e3670448
mounikakoripi <31993587+mounikakoripi@users.noreply.github.com>
2023-06-06 22:28:09 +0530

    Add files via upload

---
+++ JFM1T2_Assignment-mounikakoripi/Operations.java
@@ -0,0 +1,34 @@
+/*  JFM1T2_Assignment1:
+
+     Write a program to perform the below actions and print the result.
+     1. Difference of 2 numbers (30 and 10)
+     2. Product of 2 numbers (45 and 2)
+     3. Division of 2 numbers (600 and 10)
+     4. Increment and Decrement the number (15)
+     5. Remainder of 2 numbers (14 and 5)
+     Prompt the user input from the terminal.
+*/
+
+//import statements for java program to read inputs using Scanner class
+import java.util.Scanner;
+
+public class Operations {
+
+//main method
+
+/*
+  Use the scanner class to provide input at execution time using scanner object
+  Scanner sc=new Scanner(System.in);
+*/
+
+/*
+  Take input from user
+  System.out.println("Enter first number: ");
+  int a=sc.nextInt();
+*/
+
+//calculate difference,product,division,increment and remainder 
+
+//print appropriate result for all operations
+
+}
